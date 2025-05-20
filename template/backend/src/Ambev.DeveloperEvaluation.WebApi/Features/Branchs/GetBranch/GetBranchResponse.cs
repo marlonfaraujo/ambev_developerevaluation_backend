28ed5bc1dd92461efb7183b692f70c09129965d1
Marlon <marlonfda@gmail.com>
2025-05-20 00:24:26 -0300

    branch endpoint and integration tests for the controller

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Branchs/GetBranch/GetBranchResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Branchs.GetBranch
+{
+    public record GetBranchResponse(Guid Id, string Name, string Description)
+    {
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Branchs/ListBranchs/ListBranchsRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Branchs.ListBranchs
+{
+    public record ListBranchsRequest(Guid Id, string Name, string Description)
+    {
+    }
+}
