679cf9176cdbca3747cd419ff4e2ec88f867965f
Marlon <marlonfda@gmail.com>
2025-08-23 16:42:02 -0300

    feature(sale-integration): adding redis to project for shopping cart cache

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
@@ -4,11 +4,10 @@ namespace Ambev.DeveloperEvaluation.Domain.Entities
 {
     public class Product : BaseEntity
     {
-        public Guid Id { get; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public Product(string id, string name, string description, decimal price)
+        public Product(Guid id, string name, string description, decimal price)
         {
             Id = id;
             Name = name;
