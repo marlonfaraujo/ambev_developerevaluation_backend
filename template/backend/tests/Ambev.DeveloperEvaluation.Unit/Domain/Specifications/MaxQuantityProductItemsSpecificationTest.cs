a259fd062f586fcf7b7635e355d5959692d17a62
Marlon <marlonfda@gmail.com>
2025-08-23 18:25:10 -0300

    feature(sale-integration): more unit tests for the max items per product rule

---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Domain/Specifications/MaxQuantityProductItemsSpecificationTest.cs
@@ -0,0 +1,25 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Specifications;
+using Ambev.DeveloperEvaluation.Unit.Domain.Entities.TestData;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.Specifications
+{
+    public class MaxQuantityProductItemsSpecificationTest
+    {
+        [Fact(DisplayName = "MaxQuantityProductItemsSpecificationTest")]
+        public void Given_QuantityOfItems_When_ItIsGreaterThan20_Then_ErrorMustOccur()
+        {
+            var spec = new MaxQuantityProductItemsSpecification();
+
+            Guid productId = SaleItemTestData.GenerateValidProductId();
+            int itemQuantity = SaleItemTestData.GenerateValidItemQuantity(20,50);
+            decimal price = SaleItemTestData.GenerateValidUnitItemPrice();
+            var saleItems = new List<SaleItem> {
+                SaleItemTestData.GenerateValidSaleItem(productId, itemQuantity, price)
+            };
+
+            Assert.Equal(spec.IsSatisfiedBy(saleItems), false);
+        }
+    }
+}
