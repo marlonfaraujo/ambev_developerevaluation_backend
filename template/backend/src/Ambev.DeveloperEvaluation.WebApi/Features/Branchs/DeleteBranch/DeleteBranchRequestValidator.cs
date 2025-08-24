3ea592ecf517473a5f9e828b1562d27652afb70c
Marlon <marlonfda@gmail.com>
2025-08-24 01:53:11 -0300

    feature(sales-params-crud): branch endpoint and integration tests for the controller

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Branchs/DeleteBranch/DeleteBranchRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Branchs.DeleteBranch
+{
+    public class DeleteBranchRequestValidator : AbstractValidator<DeleteBranchRequest>
+    {
+        public DeleteBranchRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Id is required.");
+        }
+    }
+}
